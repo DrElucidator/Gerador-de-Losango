4c45d4ff60188a2f36291158689341fed9c5f9e4
Alec Luí <dr.elucidator@gmail.com>
2026-03-26 16:45:36 -0300

    first commit

---
+++ Losango.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] arg)
+    {
+        int Input;
+        do
+        {
+            System.Console.WriteLine("Digite um número ímpar para gerar um losango do tamanho correspondente:");
+            Input = Convert.ToInt32(Console.ReadLine());
+
+            if (Input % 2 == 0)
+            {
+                System.Console.WriteLine("Digite um número válido");
+                continue;
+            }
+        } while (Input % 2 == 0);
+        int UpperLine = (Input - 1) / 2;
+        int SizeX = 1;
+        int SizeY = UpperLine;
+        for (int i = 0; i <= UpperLine; i++)
+        {
+            Console.Write(new string(' ', SizeY * 2));
+            for (int x = 0; x < SizeX; x++)
+            {
+                Console.Write("X");
+                if (x < SizeX - 1) Console.Write(" ");
+            }
+            Console.WriteLine();
+            SizeX += 2;
+            SizeY -= 1;
+        }
+        SizeX = Input - 2;
+        SizeY = 1;
+        for (int i = 0; i < UpperLine; i++)
+        {
+            Console.Write(new string(' ', SizeY * 2));
+            for (int x = 0; x < SizeX; x++)
+            {
+                Console.Write("X");
+                if (x < SizeX - 1) Console.Write(" ");
+            }
+            Console.WriteLine();
+
+            SizeX -= 2;
+            SizeY += 1;
+        }
+    }
+}
